a846b4f2ed911beaf605a5650882b3b119024066
Anthony Forsythe <anthony.forsythe@target.com>
2019-02-13 16:00:10 -0600

    Adds good input data

--- classifier_api/src/main/java/com/hackathon/emojifier/Utilities/ClassifierConfiguration.java
+++ classifier_api/src/main/java/com/hackathon/emojifier/Utilities/ClassifierConfiguration.java
@@ -2,6 +2,6 @@ package com.hackathon.emojifier.Utilities;
 
 public class ClassifierConfiguration {
 
-    public static String EPOCHS = "30";
+    public static String EPOCHS = "100";
     public static String LEARNING_RATE = "0.8";
 }
